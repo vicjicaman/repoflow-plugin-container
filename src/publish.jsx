5001fa0b0e4afce9f15d78364dd03cc1b5780563
Victor Jimenez <vic.jicama@gmail.com>
2019-01-13 22:47:10 -0600

    Change publish to generic use release/iteration

--- src/publish.jsx
+++ src/publish.jsx
@@ -4,20 +4,35 @@ import axios from 'axios'
 import {event} from './io';
 
 export const publish = async (params, cxt) => {
-  const {module: {
-      type
-    }} = params;
-  console.log('http://localbuild:8000/build/' + type);
-  const response = await axios.post('http://localbuild:8000/build/' + type, params, {responseType: 'stream'});
+  const {
+    keyPath,
+    module: {
+      moduleid,
+      type,
+      mode,
+      version,
+      fullname,
+      url,
+      commitid
+    }
+  } = params;
+
+  const response = await axios.post('http://localbuild:8000/build/' + type, {
+    moduleid,
+    type,
+    mode,
+    version,
+    fullname,
+    url,
+    commitid,
+    keyPath
+  }, {responseType: 'stream'});
 
   let publishStreamFinished = false;
-  //response.data.pipe(process.stdout);
-
   response.data.on('data', (data) => {
     event("publish.out", {
       data
     }, cxt);
-
   });
   response.data.on('end', function() {
     publishStreamFinished = true;
@@ -25,18 +40,9 @@ export const publish = async (params, cxt) => {
     event("publish.finished", {}, cxt);
   });
 
-  //console.log(JSON.stringify(queryLocal.data));
-  //const res = queryLocal.data;
-
-  //if (!res.success) {
-  //throw new Error(res.error);
-  //}
-
   while (publishStreamFinished === false) {
     await wait(100);
   }
-
-  //throw new Error("FORCED");
 
   return {stdout: "published", stderr: ""};
 }
