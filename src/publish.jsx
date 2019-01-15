8aa5ef1402a02b4b4d8cc480a45ab16c959983eb
Victor Jimenez <vic.jicama@gmail.com>
2019-01-14 23:56:58 -0600

    Fix issue when publishing npm and container

--- src/publish.jsx
+++ src/publish.jsx
@@ -13,7 +13,8 @@ export const publish = async (params, cxt) => {
       version,
       fullname,
       url,
-      commitid
+      commitid,
+      branchid
     }
   } = params;
 
@@ -25,23 +26,53 @@ export const publish = async (params, cxt) => {
     fullname,
     url,
     commitid,
+    branchid,
     keyPath
   }, {responseType: 'stream'});
 
   let publishStreamFinished = false;
-  response.data.on('data', (data) => {
-    event("publish.out", {
-      data
+  let publishStreamError = false;
+
+  response.data.on('error', (data) => {
+    console.log("STREAM_PUBLISH_ERROR");
+    publishStreamError = data.toString();
+    event("publish.error", {
+      data: data.toString()
     }, cxt);
   });
+
+  response.data.on('data', (raw) => {
+    console.log("STREAM_PUBLISH_OUTPUT");
+    const rawString = raw.toString();
+    let data = {};
+
+    try {
+      data = JSON.parse(raw.toString())
+    } catch (e) {
+      console.log("STREAM_PUBLISH_PARSE:" + rawString);
+    }
+
+    if (data.error) {
+      publishStreamError = data.error;
+    }
+
+    event("publish.out", {
+      data: rawString
+    }, cxt);
+
+  });
+
   response.data.on('end', function() {
     publishStreamFinished = true;
-    console.log('finished');
     event("publish.finished", {}, cxt);
   });
 
   while (publishStreamFinished === false) {
     await wait(100);
+  }
+
+  if (publishStreamError) {
+    return {stdout: "", stderr: publishStreamError};
   }
 
   return {stdout: "published", stderr: ""};
